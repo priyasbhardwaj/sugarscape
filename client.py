204e1a4500c43842784c5529b892956f0b8096aa
Elijah Parker <elijahparker7@gmail.com>
2024-03-06 10:22:24 -0800

    Implemented p2p connections to send basic heartbeat message each minute

--- client.py
+++ client.py
@@ -1,10 +1,20 @@
 import socket
+import time
 
-LOCAL_SERVER = 'localhost'
-SERVER_PORT = 5555
 
-def start_client():
+def start_client(host_id, host, port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((LOCAL_SERVER, SERVER_PORT))
-    client.listen()
-        
+    client.connect((host, port))
+    print(f"Connected to peer {host_id}")
+    while True:
+        msg = 'heartbeat'
+        client.send(msg.encode('utf-8'))
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    connections = [
+        {"host_id": 1, "host": "localhost", "port": 80}
+    ]
+    for connection in connections:
+        start_client(connection["host_id"], connection["host"], connection["port"])
--- server.py
+++ server.py
@@ -1,14 +1,33 @@
 import socket
+import threading
 
-SERVER_HOST = 'localhost'
-SERVER_PORT = 5555
-connected_servers = []
+PORT = 80
 
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((SERVER_HOST, SERVER_PORT))
+    server.bind(('0.0.0.0', PORT))
     server.listen()
-    
-    print(f"Server listening on {SERVER_HOST}:{SERVER_PORT}")
+    print(f"Server listening on port: {PORT}")
+
+    while True:
+        client, address = server.accept()
+        client_listener = threading.Thread(target=listener, args=(client, address))
+        client_listener.start()
+
+
+def listener(client, address):
+    print(f"Connection established with {address}")
+
+    while True:
+        msg = client.recv(1024)
+        if not msg:
+            break
+        print(f"Received message from {address}: {msg.decode('utf-8')}")
+    print(f"Connection closed")
+
+
+if __name__ == "__main__":
+    start_server()
+
     
